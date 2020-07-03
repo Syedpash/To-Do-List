291654d1bb249a6276c74849791627edeefd7c14
Syed <syed97syed97@gmail.com>
2020-07-03 20:41:47 +0530

    Updates

---
+++ precache-manifest.7fba4017afb482b06e9a8a62b880f4b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0d36a49ebf76ed7ddb85f3ee0cc0ff",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "61e99c64c05bccf827ef",
+    "url": "/To-Do-List/static/css/main.362672fb.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/To-Do-List/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e99c64c05bccf827ef",
+    "url": "/To-Do-List/static/js/main.1e3fd05a.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
